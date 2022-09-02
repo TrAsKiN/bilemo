ea1e9314a7b1797e1de319f013fb4e19af80ba00
TrAsKiN <marchal.simon@gmail.com>
2022-09-02 16:09:26 +0200

    add product controller

---
+++ README.md
@@ -0,0 +1,1 @@
+# BileMo [![SymfonyInsight](https://insight.symfony.com/projects/b2fde0bb-e127-454f-894a-ae059e3ef385/mini.svg)](https://insight.symfony.com/projects/b2fde0bb-e127-454f-894a-ae059e3ef385)
--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
