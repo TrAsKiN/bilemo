0d0d1ab8965cdba9d43d2c61f82700e7883b2d5d
TrAsKiN <marchal.simon@gmail.com>
2022-09-06 16:19:07 +0200

    add customer fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,10 +9,13 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    public const USER_REFERENCE = 'user';
+
     public function __construct(
         private readonly UserPasswordHasherInterface $passwordHasher
     ) {
     }
+
     public function load(ObjectManager $manager): void
     {
         $user = (new User())
@@ -21,8 +24,18 @@ class UserFixtures extends Fixture
             ->setSiren(12345678)
         ;
         $user->setPassword($this->passwordHasher->hashPassword($user, 'test'));
-        $manager->persist($user);
+        $manager->getRepository(User::class)->add($user);
+
+        $userTest = (new User())
+            ->setEmail('test@example.com')
+            ->setCompanyName('Test Company')
+            ->setSiren(87654321)
+        ;
+        $userTest->setPassword($this->passwordHasher->hashPassword($userTest, 'example'));
+        $manager->getRepository(User::class)->add($userTest);
 
         $manager->flush();
+        $this->addReference(self::USER_REFERENCE, $user);
+        $this->addReference(self::USER_REFERENCE . 'Test', $userTest);
     }
 }
