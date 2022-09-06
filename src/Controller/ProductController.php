58437221d662bc6f0fbc0e025b818048702e14b7
TrAsKiN <marchal.simon@gmail.com>
2022-09-06 16:20:56 +0200

    improve naming consistency

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,9 +5,6 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\PaginatorService;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
-use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,20 +13,20 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/products')]
 class ProductController extends AbstractController
 {
-    #[Route('/', name: 'app_products', methods: Request::METHOD_GET)]
-    public function products(
+    #[Route('/', name: 'app_products_list', methods: [Request::METHOD_GET])]
+    public function productsList(
         Request $request,
         ProductRepository $productRepository,
         PaginatorService $paginator
     ): JsonResponse {
         $page = $request->query->getInt('page', 1);
-        $products = $productRepository->getPaginateProducts($page, ProductRepository::MAX_PER_PAGE);
-        $result = $paginator->paginate($products, 'app_products', $page, ProductRepository::MAX_PER_PAGE);
+        $products = $productRepository->getPaginateProducts($page);
+        $result = $paginator->paginate($products, 'app_products_list', $page, ProductRepository::MAX_PER_PAGE);
         return $this->json($result);
     }
 
-    #[Route('/{id}', name: 'app_product', methods: Request::METHOD_GET)]
-    public function product(
+    #[Route('/{id}', name: 'app_products_show', methods: [Request::METHOD_GET])]
+    public function productsShow(
         Product $product
     ): JsonResponse {
         return $this->json($product);
